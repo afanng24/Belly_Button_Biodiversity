f5d275bceb2235069f5440f0473f50d73f157370
Alan Luo Fanng <afanng24@gmail.com>
2021-07-11 15:45:39 -0400

    Add files via upload

---
+++ LoadTest/plots.js
@@ -0,0 +1,21 @@
+
+d3.json("samples.json").then(function(data){
+    console.log("hello");
+});
+d3.json("samples.json").then(function(data){
+    wfreq = data.metadata.map(person => person.wfreq);
+    console.log(wfreq);
+});
+
+d3.json("samples.json").then(function(data){
+    wfreq = data.metadata.map(person =>
+person.wfreq).sort((a,b) => b - a);
+    console.log(wfreq);
+});
+d3.json("samples.json").then(function(data){
+    wfreq = data.metadata.map(person =>
+person.wfreq).sort((a,b) => b - a);
+    filteredWfreq = wfreq.filter(element => element !=
+null);
+    console.log(filteredWfreq);
+});
