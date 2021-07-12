3822f2c83176ac9b63f655f5fd365a6a708eba3d
Alan Luo Fanng <afanng24@gmail.com>
2021-07-12 09:59:35 -0400

    Add files via upload

--- js/charts.js
+++ js/charts.js
@@ -89,6 +89,6 @@ function buildCharts(sample) {
       title: "Top 10 Bacteria Cultures Found"
     };
     // 10. Use Plotly to plot the data with the layout. 
-    Plotly.newPlot("bar, barData, barLayout");
+    Plotly.newPlot("bar", barData, barLayout);
   });
 }
