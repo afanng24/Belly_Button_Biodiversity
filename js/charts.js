abeef30e3ee5ea84472072d546f0859f47484598
Alan Luo Fanng <afanng24@gmail.com>
2021-07-12 10:03:09 -0400

    Add files via upload

--- js/charts.js
+++ js/charts.js
@@ -82,7 +82,7 @@ function buildCharts(sample) {
       y: yticks,
       type: "bar",
       orientation: "h",
-      text: labels 
+      //text: labels 
     }];
     // 9. Create the layout for the bar chart. 
     var barLayout = {
